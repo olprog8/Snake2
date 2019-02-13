e428d044b4979b46a91f14675cf7d5c610ff2954
olprog8 <solar2010@live.ru>
2019-02-13 13:44:57 +0300

    Создали класс Snake который отрисовывает Змейку

--- Snake2/Snake2/Point.cs
+++ Snake2/Snake2/Point.cs
@@ -19,10 +19,43 @@ namespace Snake2
             sym = _sym;
         }
 
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
+
+        public void Move(int offset, Direction direction)
+        {
+            if(direction == Direction.RIGHT)
+            {
+                x = x + offset;
+            }
+            if (direction == Direction.LEFT)
+            {
+                x = x - offset;
+            }
+            if (direction == Direction.UP)
+            {
+                y = y + offset;
+            }
+            if (direction == Direction.DOWN)
+            {
+                y = y - offset;
+            }
+
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
+        }
+
+        public override string ToString()
+        {
+            return x + ", " + y + ", " + sym;
         }
     }
 }
--- Snake2/Snake2/Program.cs
+++ Snake2/Snake2/Program.cs
@@ -25,7 +25,8 @@ namespace Snake2
             rightLine.Drow();
 
             Point p = new Point(4,5,'*');
-            p.Draw();
+
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
 
 
             Console.ReadLine();
