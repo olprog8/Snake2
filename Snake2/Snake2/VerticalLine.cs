1f31dad047fc31348abb21cdfad5e22aaeda9d57
olprog8 <solar2010@live.ru>
2019-02-14 12:59:29 +0300

    Полная версия

---
+++ Snake2/Snake2/VerticalLine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake2
+{
+    //Наследование - это созвучно с понятием ЯВЛЯЕТСЯ частью
+    class VerticalLine : Figure
+    {
+
+        public VerticalLine(int yFirst, int yLast, int x, char sym)
+        {
+
+            pList = new List<Point>();
+
+            for (int y = yFirst; y <= yLast; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+
+        }
+
+    }
+}
